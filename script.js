a3a6fc3ea8b735d21e06bd3c544eccc7c284f868
Le Duong - Mac <leduongnewmemes123@gmail.com>
2025-05-09 16:23:52 +0700

    Finish basic math function

---
+++ script.js
@@ -0,0 +1,18 @@
+// basic math functions
+function add(num1, num2) {
+    return num1 + num2;
+}
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+function divide(num1, num2) {
+    return num1 / num2
+}
+
+console.log(add(10, 2))
+console.log(subtract(10, 2))
+console.log(multiply(10, 2))
+console.log(divide(10, 2))
