969f57e988ce35f64fae745b46ec57a771ae0795
Le Duong - Mac <leduongnewmemes123@gmail.com>
2025-05-09 16:33:51 +0700

    Finish operator function to operator basic maths

--- script.js
+++ script.js
@@ -16,3 +16,16 @@ console.log(add(10, 2))
 console.log(subtract(10, 2))
 console.log(multiply(10, 2))
 console.log(divide(10, 2))
+
+// operation function
+const number1 = null;
+const number2 = null;
+const operator = null; 
+
+function operate(operator, num1, num2) {
+    if (operator === '+') return add(num1, num2);
+    else if (operator === '-') return subtract(num1, num2);
+    else if (operator === '*') return multiply(num1, num2);
+    else if (operator === '/') return divide(num1, num2);    
+}
+
